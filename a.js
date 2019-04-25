dda73ae1212f5350651cc988cf655ecbd42b0d1d
Huong <huongnguyenak96@gmail.com>
2019-04-25 20:29:05 +0700

    obj 2

--- a.js
+++ a.js
@@ -12,25 +12,42 @@ class Person{
         this.name = name;
         this.height = height;
     }
+
+    /**
+     * @returns {string}
+     */
     getName(){
         return this.name;
     }
-}
-// const ti = new Person('Ti', 160);
-// console.log(ti);
-
-function Product(name, price){
-    this.name = name;
-    this.price = price;
-    function getName(){
-        return this.name;
+    /**
+     * 
+     * @param {string | number} newName 
+     * @returns {void}
+     */
+    setName(newName){
+        this.name = newName;
     }
 }
-function getName(){
-    return 'My name is KPT';
-}
+const ti = new Person('Ti', 160);
+// console.log(ti.setName('Ti Nguyen'));
+// console.log(ti.name);
+// console.log(ti.getName());
+// ti.name = 'Ti Nguyen';
+// console.log(ti)
 
-console.log(getName)
+
+// function Product(name, price){
+//     this.name = name;
+//     this.price = price;
+//     function getName(){
+//         return this.name;
+//     }
+// }
+// function getName(){
+//     return 'My name is KPT';
+// }
+
+// console.log(getName)
 
 // const p = new Product('Name ABC', 20000);
 // console.log(p);
